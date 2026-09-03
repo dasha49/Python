46820a69ac0c8da32fec8627a59f460ba34613d9
dasha49 <gridkodasha@gmail.com>
2026-09-03 09:46:15 +0300

    calculator

--- first.py
+++ first.py
@@ -1,1 +1,23 @@
-print ("hello, word")
+def calculate(a: float, b: float, operation: str):
+    if operation == '+':
+        return a + b
+    elif operation == '-':
+        return a - b
+    elif operation == '*':
+        return a * b
+    elif operation == '/':
+        if b == 0:
+            return "Помилка: ділення на нуль неможливе"
+        return a / b
+    else:
+        return "Помилка: непідтримувана операція"
+
+try:
+    num1 = float(input("Введіть перше число: "))
+    op = input("Введіть операцію (+, -, *, /): ").strip()
+    num2 = float(input("Введіть друге число: "))
+
+    result = calculate(num1, num2, op)
+    print(f"Результат: {result}")
+except ValueError:
+    print("Помилка: необхідно ввести числове значення")
